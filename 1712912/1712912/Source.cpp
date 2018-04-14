6fdb857a4b0666370d5db475e2ced4884b2e3679
Nguyen Hoang Vinh <hoangvinh175@gmail.com>
2018-04-14 15:55:18 +0700

    Thay doi lan 5

--- 1712912/1712912/Source.cpp
+++ 1712912/1712912/Source.cpp
@@ -4,6 +4,7 @@
 #include <locale.h>
 #include <io.h> 
 #include <fcntl.h>
+#include <string.h>
 
 int const field = 15;
 
@@ -15,7 +16,7 @@ struct SV
 	int khoatuyen;
 	char* ngaysinh;
 	char* email;
-	wchar_t* hinhanhcanhan; //link dẫn đến
+	wchar_t* hinhanhcanhan;
 	wchar_t* sothich1;
 	wchar_t* sothich2;
 	wchar_t* motabanthan;
@@ -28,7 +29,14 @@ void DocFile(FILE *fpin, SV **dssv, int *sosinhvien, int ***sokitu)
 	fseek(fpin, 3L, SEEK_CUR);
 	bool flag = false;
 	fwscanf(fpin, L"%d", sosinhvien);
-	fseek(fpin, 2L, SEEK_CUR);
+	rewind(fpin);
+	fseek(fpin, 3L, SEEK_CUR);
+	while (1)
+	{
+		wc = fgetwc(fpin);
+		if (wc == L'\"')    //Di chuyển tới dấu ngoặc kép của sinh viên đầu tiên, trường đầu tiên
+		    break;
+	}
 	*dssv = (SV*)malloc(*sosinhvien*sizeof(SV));
 	*sokitu = (int**)malloc((*sosinhvien)*sizeof(int*));
 	**sokitu = (int*)malloc(field*sizeof(int));
@@ -37,7 +45,7 @@ void DocFile(FILE *fpin, SV **dssv, int *sosinhvien, int ***sokitu)
 		wc = fgetwc(fpin);
 		tempt[demkitu] = wc;
 		demkitu++;
-		if (wc == L',' || wc == L'\n' || wc == WEOF)
+		if (wc == L'\"')
 		{
 			count++;
 			flag = true;
@@ -142,145 +150,165 @@ void DocFile(FILE *fpin, SV **dssv, int *sosinhvien, int ***sokitu)
 				demsosinhvien++;
 				count = 0;
 				demkitu = 0;
-				if (wc == WEOF)
+				if (demsosinhvien == *sosinhvien)
 					break;
 				*((*sokitu) + demsosinhvien) = (int*)malloc(field*sizeof(int));
+			}
+			while (1)
+			{
+				wc = fgetwc(fpin);
+				if (wc == L'\"')   //Di chuyển đến dấu ngoặc kép của trường tiếp theo
+					break;
 			}
 			flag = false;
 		}
 	}
 }
 
-void GhiFile(FILE ** fpout, SV ttsv, int *sokitu)
+void GhiFile(SV ttsv, int *sokitu)
 {
-	fwprintf(*fpout,L"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
-	fwprintf(*fpout, L"<html xmlns=\"http://www.w3.org/1999/xhtml\">\n");
-	fwprintf(*fpout, L"	<head>\n");
-	fwprintf(*fpout, L"		<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n");
-	fwprintf(*fpout, L"		<link rel=\"stylesheet\" type=\"text/css\" href=\"personal.css\" />\n");
-	fwprintf(*fpout, L"		<title>HCMUS - ");
+	FILE *fpout;
+	char *tenfile;
+	tenfile = (char*)malloc((15 + sokitu[1])*sizeof(char));
+	strcpy(tenfile, "Website//");
+	strcat(tenfile, ttsv.mssv);
+	tenfile[0 + 9 + sokitu[1]] = '.';
+	tenfile[1 + 9 + sokitu[1]] = 'h';
+	tenfile[2 + 9 + sokitu[1]] = 't';
+	tenfile[3 + 9 + sokitu[1]] = 'm';
+	tenfile[4 + 9 + sokitu[1]] = 'l';
+	tenfile[5 + 9 + sokitu[1]] = '\0';
+	fpout = fopen(tenfile, "w+");
+	_setmode(_fileno(fpout), _O_U8TEXT);
+	fwprintf(fpout, L"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
+	fwprintf(fpout, L"<html xmlns=\"http://www.w3.org/1999/xhtml\">\n");
+	fwprintf(fpout, L"	<head>\n");
+	fwprintf(fpout, L"		<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n");
+	fwprintf(fpout, L"		<link rel=\"stylesheet\" type=\"text/css\" href=\"personal.css\" />\n");
+	fwprintf(fpout, L"		<title>HCMUS - ");
 	for (int i = 0; i < sokitu[2]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.hovaten[i]);
-	fwprintf(*fpout, L"</title>\n");
-	fwprintf(*fpout, L"	</head>\n");
-	fwprintf(*fpout, L"	<body>\n");
-	fwprintf(*fpout, L"		<div class=\"Layout_container\">\n");
-	fwprintf(*fpout, L"			<!-- Begin Layout Banner Region -->\n");
-	fwprintf(*fpout, L"			<div class=\"Layout_Banner\" >\n");
-	fwprintf(*fpout, L"				<div><img id=\"logo\" src=\"Images/logo.jpg\" height=\"105\" /></div>\n");
-	fwprintf(*fpout, L"				<div class=\"Header_Title\">TRƯỜNG ĐẠI HỌC KHOA HỌC TỰ NHIÊN</div>\n");
-	fwprintf(*fpout, L"			</div>\n");
-	fwprintf(*fpout, L"			<!-- End Layout Banner Region -->\n");
-	fwprintf(*fpout, L"			<!-- Begin Layout MainContents Region -->\n");
-	fwprintf(*fpout, L"			<div class=\"Layout_MainContents\">\n");
-	fwprintf(*fpout, L"				<!-- Begin Below Banner Region -->\n");
-	fwprintf(*fpout, L"				<div class=\"Personal_Main_Information\">\n");
-	fwprintf(*fpout, L"					<!-- Begin Thông tin cá nhân của sinh viên ------------------------------------------------------------------------------------------->\n");
-	fwprintf(*fpout, L"					<div class=\"Personal_Location\"> \n");
-	fwprintf(*fpout, L"						<img src=\"Images/LogoFooter.jpg\" width=\"27\" height=\"33\" />\n");
-	fwprintf(*fpout, L"						<span class=\"Personal_FullName\">");
+		fwprintf(fpout, L"%lc", ttsv.hovaten[i]);
+	fwprintf(fpout, L"</title>\n");
+	fwprintf(fpout, L"	</head>\n");
+	fwprintf(fpout, L"	<body>\n");
+	fwprintf(fpout, L"		<div class=\"Layout_container\">\n");
+	fwprintf(fpout, L"			<!-- Begin Layout Banner Region -->\n");
+	fwprintf(fpout, L"			<div class=\"Layout_Banner\" >\n");
+	fwprintf(fpout, L"				<div><img id=\"logo\" src=\"Images/logo.jpg\" height=\"105\" /></div>\n");
+	fwprintf(fpout, L"				<div class=\"Header_Title\">TRƯỜNG ĐẠI HỌC KHOA HỌC TỰ NHIÊN</div>\n");
+	fwprintf(fpout, L"			</div>\n");
+	fwprintf(fpout, L"			<!-- End Layout Banner Region -->\n");
+	fwprintf(fpout, L"			<!-- Begin Layout MainContents Region -->\n");
+	fwprintf(fpout, L"			<div class=\"Layout_MainContents\">\n");
+	fwprintf(fpout, L"				<!-- Begin Below Banner Region -->\n");
+	fwprintf(fpout, L"				<div class=\"Personal_Main_Information\">\n");
+	fwprintf(fpout, L"					<!-- Begin Thông tin cá nhân của sinh viên ------------------------------------------------------------------------------------------->\n");
+	fwprintf(fpout, L"					<div class=\"Personal_Location\"> \n");
+	fwprintf(fpout, L"						<img src=\"Images/LogoFooter.jpg\" width=\"27\" height=\"33\" />\n");
+	fwprintf(fpout, L"						<span class=\"Personal_FullName\">");
 	for (int i = 0; i < sokitu[2]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.hovaten[i]);
-	fwprintf(*fpout, L" - ");
+		fwprintf(fpout, L"%lc", ttsv.hovaten[i]);
+	fwprintf(fpout, L" - ");
 	for (int i = 0; i < sokitu[1]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.mssv[i]);
-	fwprintf(*fpout, L"</span>\n");
-	fwprintf(*fpout, L"						<div class=\"Personal_Department\">KHOA CÔNG NGHỆ THÔNG TIN</div>\n");
-	fwprintf(*fpout, L"						<br />\n");
-	fwprintf(*fpout, L"						<div class=\"Personal_Phone\">\n");
-	fwprintf(*fpout, L"                                 Email: ");
+		fwprintf(fpout, L"%lc", ttsv.mssv[i]);
+	fwprintf(fpout, L"</span>\n");
+	fwprintf(fpout, L"						<div class=\"Personal_Department\">KHOA CÔNG NGHỆ THÔNG TIN</div>\n");
+	fwprintf(fpout, L"						<br />\n");
+	fwprintf(fpout, L"						<div class=\"Personal_Phone\">\n");
+	fwprintf(fpout, L"                                 Email: ");
 	for (int i = 0; i < sokitu[6]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.email[i]);
-	fwprintf(*fpout, L"\n");
-	fwprintf(*fpout, L"						</div>\n");
-	fwprintf(*fpout, L"						<br />\n");
-	fwprintf(*fpout, L"						<br />\n");
-	fwprintf(*fpout, L"					</div>\n");
-	fwprintf(*fpout, L"					<!-- End Thông tin cá nhân của sinh viên ------------------------------------------------------------------------------------------->\n");
-	fwprintf(*fpout, L"					<div class=\"Personal_HinhcanhanKhung\">\n");
-	fwprintf(*fpout, L"						<img src=\"Images/");
+		fwprintf(fpout, L"%lc", ttsv.email[i]);
+	fwprintf(fpout, L"\n");
+	fwprintf(fpout, L"						</div>\n");
+	fwprintf(fpout, L"						<br />\n");
+	fwprintf(fpout, L"						<br />\n");
+	fwprintf(fpout, L"					</div>\n");
+	fwprintf(fpout, L"					<!-- End Thông tin cá nhân của sinh viên ------------------------------------------------------------------------------------------->\n");
+	fwprintf(fpout, L"					<div class=\"Personal_HinhcanhanKhung\">\n");
+	fwprintf(fpout, L"						<img src=\"Images/");
 	for (int i = 0; i < sokitu[7]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.hinhanhcanhan[i]);
-	fwprintf(*fpout, L"\" class=\"Personal_Hinhcanhan\" />\n");
-	fwprintf(*fpout, L"					</div>\n");
-	fwprintf(*fpout, L"				</div>\n");
-	fwprintf(*fpout, L"				<!-- End Below Banner Region -->\n");
-	fwprintf(*fpout, L"				<!-- Begin MainContents Region -->\n");
-	fwprintf(*fpout, L"				<div class=\"MainContain\">\n");
-	fwprintf(*fpout, L"\n");
-	fwprintf(*fpout, L"					<!-- Begin Top Region -->\n");
-	fwprintf(*fpout, L"					<div class=\"MainContain_Top\">\n");
-	fwprintf(*fpout, L"\n");
-	fwprintf(*fpout, L"						<div class=\"InfoGroup\">Thông tin cá nhân</div>\n");
-	fwprintf(*fpout, L"                       <div>\n");
-	fwprintf(*fpout, L"                            <ul class=\"TextInList\">\n");
-	fwprintf(*fpout, L"                              <li>Họ và tên: ");
+		fwprintf(fpout, L"%lc", ttsv.hinhanhcanhan[i]);
+	fwprintf(fpout, L"\" class=\"Personal_Hinhcanhan\" />\n");
+	fwprintf(fpout, L"					</div>\n");
+	fwprintf(fpout, L"				</div>\n");
+	fwprintf(fpout, L"				<!-- End Below Banner Region -->\n");
+	fwprintf(fpout, L"				<!-- Begin MainContents Region -->\n");
+	fwprintf(fpout, L"				<div class=\"MainContain\">\n");
+	fwprintf(fpout, L"\n");
+	fwprintf(fpout, L"					<!-- Begin Top Region -->\n");
+	fwprintf(fpout, L"					<div class=\"MainContain_Top\">\n");
+	fwprintf(fpout, L"\n");
+	fwprintf(fpout, L"						<div class=\"InfoGroup\">Thông tin cá nhân</div>\n");
+	fwprintf(fpout, L"                       <div>\n");
+	fwprintf(fpout, L"                            <ul class=\"TextInList\">\n");
+	fwprintf(fpout, L"                              <li>Họ và tên: ");
 	for (int i = 0; i < sokitu[2]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.hovaten[i]);
-	fwprintf(*fpout, L" </li>\n");
-	fwprintf(*fpout, L"								 <li>MSSV: ");
+		fwprintf(fpout, L"%lc", ttsv.hovaten[i]);
+	fwprintf(fpout, L" </li>\n");
+	fwprintf(fpout, L"								 <li>MSSV: ");
 	for (int i = 0; i < sokitu[1]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.mssv[i]);
-	fwprintf(*fpout, L" </li>\n");
-	fwprintf(*fpout, L"								 <li>Sinh viên khoa ");
+		fwprintf(fpout, L"%lc", ttsv.mssv[i]);
+	fwprintf(fpout, L" </li>\n");
+	fwprintf(fpout, L"								 <li>Sinh viên khoa ");
 	for (int i = 0; i < sokitu[3]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.khoa[i]);
-	fwprintf(*fpout, L" </li>\n");
-	fwprintf(*fpout, L"								 <li>Khóa ");
-	fwprintf(*fpout, L"%d", ttsv.khoatuyen);
-	fwprintf(*fpout, L" </li>\n");
-	fwprintf(*fpout, L"								 <li>Ngày sinh: ");
+		fwprintf(fpout, L"%lc", ttsv.khoa[i]);
+	fwprintf(fpout, L" </li>\n");
+	fwprintf(fpout, L"								 <li>Khóa ");
+	fwprintf(fpout, L"%d", ttsv.khoatuyen);
+	fwprintf(fpout, L" </li>\n");
+	fwprintf(fpout, L"								 <li>Ngày sinh: ");
 	for (int i = 0; i < sokitu[5]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.ngaysinh[i]);
-	fwprintf(*fpout, L" </li>\n");
-	fwprintf(*fpout, L"								 <li>Email: ");
+		fwprintf(fpout, L"%lc", ttsv.ngaysinh[i]);
+	fwprintf(fpout, L" </li>\n");
+	fwprintf(fpout, L"								 <li>Email: ");
 	for (int i = 0; i < sokitu[6]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.email[i]);
-	fwprintf(*fpout, L" </li>\n");
-	fwprintf(*fpout, L"							 </ul>\n");
-	fwprintf(*fpout, L"						</div>\n");
-	fwprintf(*fpout, L"                       <div class=\"InfoGroup\">Sở thích</div>\n");
-	fwprintf(*fpout, L"                       <div>\n");
-	fwprintf(*fpout, L"                            <ul class=\"TextInList\">\n");
-	fwprintf(*fpout, L"                              <li>");
+		fwprintf(fpout, L"%lc", ttsv.email[i]);
+	fwprintf(fpout, L" </li>\n");
+	fwprintf(fpout, L"							 </ul>\n");
+	fwprintf(fpout, L"						</div>\n");
+	fwprintf(fpout, L"                       <div class=\"InfoGroup\">Sở thích</div>\n");
+	fwprintf(fpout, L"                       <div>\n");
+	fwprintf(fpout, L"                            <ul class=\"TextInList\">\n");
+	fwprintf(fpout, L"                              <li>");
 	for (int i = 0; i < sokitu[8]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.sothich1[i]);
-	fwprintf(*fpout, L"</li>\n");
-	fwprintf(*fpout, L"								 <li>");
+		fwprintf(fpout, L"%lc", ttsv.sothich1[i]);
+	fwprintf(fpout, L"</li>\n");
+	fwprintf(fpout, L"								 <li>");
 	for (int i = 0; i < sokitu[9]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.sothich2[i]);
-	fwprintf(*fpout, L"</li>\n");
-	fwprintf(*fpout, L"							 </ul>\n");
-	fwprintf(*fpout, L"						</div>\n");
-	fwprintf(*fpout, L"						<div class=\"InfoGroup\">Mô tả</div>\n");
-	fwprintf(*fpout, L"						<div class=\"Description\">\n");
-	fwprintf(*fpout, L"                            ");
+		fwprintf(fpout, L"%lc", ttsv.sothich2[i]);
+	fwprintf(fpout, L"</li>\n");
+	fwprintf(fpout, L"							 </ul>\n");
+	fwprintf(fpout, L"						</div>\n");
+	fwprintf(fpout, L"						<div class=\"InfoGroup\">Mô tả</div>\n");
+	fwprintf(fpout, L"						<div class=\"Description\">\n");
+	fwprintf(fpout, L"                            ");
 	for (int i = 0; i < sokitu[10]; i++)
-		fwprintf(*fpout, L"%lc", ttsv.motabanthan[i]);
-	fwprintf(*fpout, L".\n");
-	fwprintf(*fpout, L"						</div>\n");
-	fwprintf(*fpout, L"\n");
-	fwprintf(*fpout, L"					</div>\n");
-	fwprintf(*fpout, L"				</div>\n");
-	fwprintf(*fpout, L"			</div>\n");
-	fwprintf(*fpout, L"\n");
-	fwprintf(*fpout, L"			<!-- Begin Layout Footer -->\n");
-	fwprintf(*fpout, L"			<div class=\"Layout_Footer\">\n");
-	fwprintf(*fpout, L"				<div class=\"divCopyright\">\n");
-	fwprintf(*fpout, L"					<br />\n");
-	fwprintf(*fpout, L"					<img src=\"Images/LogoFooter_gray.jpg\" width=\"34\" height=\"41\" /><br />\n");
-	fwprintf(*fpout, L"					<br />\n");
-	fwprintf(*fpout, L"				@2018</br>\n");
-	fwprintf(*fpout, L"				Đồ án giữa kì</br>\n");
-	fwprintf(*fpout, L"				Kỹ thuật lâp trình</br>\n");
-	fwprintf(*fpout, L"				TH2017/18</br>\n");
-	fwprintf(*fpout, L"				1712912 - Nguyễn Hoàng Vinh</br>\n");
-	fwprintf(*fpout, L"				</div>\n");
-	fwprintf(*fpout, L"			</div>\n");
-	fwprintf(*fpout, L"			<!-- End Layout Footer -->\n");
-	fwprintf(*fpout, L"		</div>\n");
-	fwprintf(*fpout, L"	</body>\n");
-	fwprintf(*fpout, L"</html>");
+		fwprintf(fpout, L"%lc", ttsv.motabanthan[i]);
+	fwprintf(fpout, L".\n");
+	fwprintf(fpout, L"						</div>\n");
+	fwprintf(fpout, L"\n");
+	fwprintf(fpout, L"					</div>\n");
+	fwprintf(fpout, L"				</div>\n");
+	fwprintf(fpout, L"			</div>\n");
+	fwprintf(fpout, L"\n");
+	fwprintf(fpout, L"			<!-- Begin Layout Footer -->\n");
+	fwprintf(fpout, L"			<div class=\"Layout_Footer\">\n");
+	fwprintf(fpout, L"				<div class=\"divCopyright\">\n");
+	fwprintf(fpout, L"					<br />\n");
+	fwprintf(fpout, L"					<img src=\"Images/LogoFooter_gray.jpg\" width=\"34\" height=\"41\" /><br />\n");
+	fwprintf(fpout, L"					<br />\n");
+	fwprintf(fpout, L"				@2018</br>\n");
+	fwprintf(fpout, L"				Đồ án giữa kì</br>\n");
+	fwprintf(fpout, L"				Kỹ thuật lâp trình</br>\n");
+	fwprintf(fpout, L"				TH2017/18</br>\n");
+	fwprintf(fpout, L"				1712912 - Nguyễn Hoàng Vinh</br>\n");
+	fwprintf(fpout, L"				</div>\n");
+	fwprintf(fpout, L"			</div>\n");
+	fwprintf(fpout, L"			<!-- End Layout Footer -->\n");
+	fwprintf(fpout, L"		</div>\n");
+	fwprintf(fpout, L"	</body>\n");
+	fwprintf(fpout, L"</html>");
+	fclose(fpout);
 }
 
 void GiaiPhong(SV **dssv, int sosinhvien, int ***sokitu)
@@ -304,54 +332,20 @@ void GiaiPhong(SV **dssv, int sosinhvien, int ***sokitu)
 
 void main()
 {
-	FILE *fpin, *file1, *file2, *file3, *file4, *file5, *file6;
-	//FILE * fpin, *file1, *file2, *file3, *file4, *file5, *file6, *file7, *file8, *file9, *file10, *arr[10] = { file1, file2, file3, file4, file5, file6, file7, file8, file9, file10 };
-	//char* tenfile[10] = { "D:\\web1.txt", "D:\\web2.txt", "Website\\web3.html", "Website\\web4.html", "Website\\web5.html", "Website\\web6.html", "Website\\web7.html", "Website\\web8.html", "Website\\web9.html", "Website\\web10.html" };
+	FILE *fpin;
 	SV *dssv;
 	int sosinhvien, dem = 0, **sokitu;
 	fpin = fopen("thongtinsinhvien.csv", "r");
-	/*
-	file1 = fopen("Website//web1.html", "w+");
-	file2 = fopen("Website//web2.html", "w+");
 	_setmode(_fileno(fpin), _O_U8TEXT);  //in tiếng việt từ những dòng tiếng việt trong fwprintf. Vd: TRƯỜNG ĐẠI HỌC KHOA HỌC TỰ NHIÊN, KHOA CÔNG NGHỆ THÔNG TIN
-	_setmode(_fileno(file1), _O_U8TEXT); //in tiếng việt từ struct sinh viên 1 lên web1
-	_setmode(_fileno(file2), _O_U8TEXT); //in tiếng việt từ struct sinh viên 2 lên web2
-	*/
-
-	_setmode(_fileno(fpin), _O_U8TEXT);
 	DocFile(fpin, &dssv, &sosinhvien, &sokitu);
+	while (dem < sosinhvien)
+	{
+		GhiFile(*(dssv + dem), *(sokitu + dem));
+		dem++;
+	}
+	if (dem == sosinhvien)
+		printf("--------Thanh cong--------\n");
 	fclose(fpin);
-
-	file1 = fopen("Website//web1.html", "w+");
-	_setmode(_fileno(file1), _O_U8TEXT);
-	GhiFile(&file1, dssv[0], *(sokitu + 0));
-	fclose(file1);
-
-	file2 = fopen("Website//web2.html", "w+");
-	_setmode(_fileno(file2), _O_U8TEXT);
-	GhiFile(&file2, dssv[1], *(sokitu + 1));
-	fclose(file2);
-
-	file3 = fopen("Website//web3.html", "w+");
-	_setmode(_fileno(file3), _O_U8TEXT);
-	GhiFile(&file3, dssv[2], *(sokitu + 2));
-	fclose(file3);
-
-	file4 = fopen("Website//web4.html", "w+");
-	_setmode(_fileno(file4), _O_U8TEXT);
-	GhiFile(&file4, dssv[3], *(sokitu + 3));
-	fclose(file4);
-
-	file5 = fopen("Website//web5.html", "w+");
-	_setmode(_fileno(file4), _O_U8TEXT);
-	GhiFile(&file5, dssv[4], *(sokitu + 4));
-	fclose(file5);
-
-	file6 = fopen("Website//web6.html", "w+");
-	_setmode(_fileno(file6), _O_U8TEXT);
-	GhiFile(&file6, dssv[5], *(sokitu + 5));
-	fclose(file6);
-
 	GiaiPhong(&dssv, sosinhvien, &sokitu);
 	system("pause");
 }
